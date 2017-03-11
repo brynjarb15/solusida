ced4a317dc8911a423fc5b2a5809ed6b9eb09b71
brynjarb15 <brynjarb15@ru.is>
2017-03-11 13:04:53 +0000

    nýjir componentar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,12 +8,18 @@ import { AppComponent } from './app.component';
 import { SellersService } from './sellers.service';
 import { SellerDlgComponent } from './seller-dlg/seller-dlg.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { ListSellersComponent } from './list-sellers/list-sellers.component';
+import { SellerDetailsComponent } from './seller-details/seller-details.component';
+import { ProductDlgComponent } from './product-dlg/product-dlg.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		SellerDlgComponent,
-		ProductCardComponent
+		ProductCardComponent,
+		ListSellersComponent,
+		SellerDetailsComponent,
+		ProductDlgComponent
 	],
 	imports: [
 		BrowserModule,
