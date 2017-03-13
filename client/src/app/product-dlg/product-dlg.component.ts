82102e362408d9e51b4717bae7745f0041904a48
berglind14 <berglind14@ru.is>
2017-03-13 12:12:53 +0000

    breytt validation á product

--- client/src/app/product-dlg/product-dlg.component.ts
+++ client/src/app/product-dlg/product-dlg.component.ts
@@ -17,15 +17,24 @@ export class Product {
 })
 export class ProductDlgComponent implements OnInit {
 
+  private product: Product;
+
   parentClass = 'has-danger';
 	childClass = 'form-control-danger';
 
-  private product: Product;
-  noName: boolean;
-  negPrice: boolean;
-  negInStock: boolean;
-  negSold: boolean;
-  validInput: boolean;
+  isNameClass = '';
+	isNameParentClass = '';
+	isPriceClass = '';
+	isPriceParentClass = '';
+	isInStockClass = '';
+	isInStockParentClass = '';
+  isSoldClass = '';
+  isSoldParentClass = '';
+
+  noName = false;
+  negPrice = false;
+  negInStock = false;
+  negSold = false;
 
   constructor(public activeModal: NgbActiveModal) { }
 
@@ -37,34 +46,53 @@ export class ProductDlgComponent implements OnInit {
   }
 
   onOk() {
-    this.validInputForProduct();
-    if(this.validInput) {
+    if(this.validInputForProduct()) {
         this.activeModal.close(this.product);
     }
   }
 
-  validInputForProduct() {
-    this.noName = false;
-    this.negPrice = false;
-    this.negInStock = false;
-    this.negSold = false;
-    this.validInput = true;
+  validInputForProduct(): boolean {  
 
     if (this.product.name === '') {
       this.noName = true;
-      this.validInput = false;
+      this.isNameParentClass = this.parentClass;
+			this.isNameClass = this.childClass;
+    }
+    else {
+      this.noName = false;
+      this.isNameParentClass = '';
+			this.isNameClass = ''; 
     }
     if (this.product.price < 0 || !isFinite(this.product.price)) {
       this.negPrice = true;
-      this.validInput = false;
+      this.isPriceParentClass = this.parentClass;
+      this.isPriceClass = this.childClass;
+    }
+    else {
+      this.negPrice = false;
+      this.isPriceParentClass = '';
+      this.isPriceClass = '';
     }
     if(this.product.quantityInStock < 0 || !isFinite(this.product.quantityInStock)) {
       this.negInStock = true;
-      this.validInput = false;
+      this.isInStockParentClass = this.parentClass;
+      this.isInStockClass = this.childClass;
+    }
+    else {
+      this.negInStock = false;
+      this.isInStockParentClass = '';
+      this.isInStockClass = '';
     }
     if(this.product.quantitySold < 0 || !isFinite(this.product.quantitySold)) {
       this.negSold = true;
-      this.validInput = false;
+      this.isSoldParentClass = this.parentClass;
+      this.isSoldClass = this.childClass;
     }
+    else {
+      this.negSold = false;
+      this.isSoldParentClass = '';
+      this.isSoldClass = '';
+    }
+    return !(this.noName || this.negPrice || this.negInStock || this.negSold);
   }
 }
