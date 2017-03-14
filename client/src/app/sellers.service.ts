19ea15d99d3257e1fdd8bd8dfe8822f3aa1fba1a
brynjarb15 <brynjarb15@ru.is>
2017-03-14 22:06:24 +0000

    taka ut i service og i html i seller details

--- client/src/app/sellers.service.ts
+++ client/src/app/sellers.service.ts
@@ -48,13 +48,6 @@ export class SellersService {
 			});
 	}
 
-	getMostPopularProduct(): Observable<SellerProduct> {
-		return this.http.get('http://localhost:5000/api/sellers')
-			.map(response => {
-				return <SellerProduct>response.json();
-			});
-	}
-
 	getSellers(): Observable<Seller[]> {
 		return this.http.get('http://localhost:5000/api/sellers')
 			.map(response => {
